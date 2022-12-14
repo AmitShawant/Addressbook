50a8addcbbb35903209add6e51ec2d936c63dd62
AmitShawant <112080425+AmitShawant@users.noreply.github.com>
2022-12-14 15:15:45 +0530

    Create variables.tf

---
+++ variables.tf
@@ -0,0 +1,24 @@
+variable "aws_region" {
+       description = "The AWS region to create things in."
+       default     = "us-east-1"
+}
+
+variable "key_name" {
+    description = " SSH keys to connect to ec2 instance"
+    default     =  "new"
+}
+
+variable "instance_type" {
+    description = "instance type for ec2"
+    default     =  "t2.micro"
+}
+
+variable "tag_name" {
+    description = "Tag Name of for Ec2 instance"
+    default     = "Test_server"
+}
+
+variable "ami_id" {
+    description = "AMI for AWS-Linux Ec2 instance"
+    default     = "ami-0b0dcb5067f052a63"
+}
