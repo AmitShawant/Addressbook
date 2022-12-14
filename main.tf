1df2226d332650f479fe0c538f59b2c3b196e65c
AmitShawant <112080425+AmitShawant@users.noreply.github.com>
2022-12-14 15:13:09 +0530

    Create main.tf

---
+++ main.tf
@@ -0,0 +1,12 @@
+provider "aws" {
+  region = var.aws_region
+}
+resource "aws_instance" "Test_Server" {
+  ami           = var.ami_id
+  key_name = var.key_name
+  instance_type = var.instance_type
+  user_data = file("docker.sh")
+  tags= {
+    Name = var.tag_name
+  }
+}
